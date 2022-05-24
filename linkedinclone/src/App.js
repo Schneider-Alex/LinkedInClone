889f12e3960d8aa9fa754c4bbaf63aeea8937674
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-05-24 17:16:25 -0400

    finishing up header
    looking to publicly host images

--- linkedinclone/src/App.js
+++ linkedinclone/src/App.js
@@ -5,7 +5,6 @@ import Header from "./Header";
 function App() {
   return (
     <div className="app">
-      <h1>Let's Build a  Linkedin CLONE</h1>
       {/*Header*/}
       <Header/>
       {/*App Body*/}
--- linkedinclone/src/Header.js
+++ linkedinclone/src/Header.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Cottage';
+import NetworkIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 import "./Header.css";
+import HeaderOption from './HeaderOption';
+
 
 function Header() {
   return (
-    <div className="Header">
-      <h2>This is the Header</h2>
+    <div className="header">
 
       <div className="header_left">
-        <img src="" alt=""></img>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt=""></img>
         <div className="header_search">
             <SearchIcon/>
             <input type="text"/>
@@ -18,6 +24,12 @@ function Header() {
 
 
       <div className="header_right">
+        <HeaderOption Icon={HomeIcon} title='Home'/>
+        <HeaderOption Icon={NetworkIcon} title='My Network'/>
+        <HeaderOption Icon={BusinessCenterIcon} title='Jobs'/>
+        <HeaderOption Icon={ChatIcon} title='Messaging'/>
+        <HeaderOption Icon={NotificationsIcon} title='Notifications'/>
+        <HeaderOption avatar='https://photos.app.goo.gl/qQofxRGcKTVNrkLn8' title='Me'/>
 
 
       </div>
